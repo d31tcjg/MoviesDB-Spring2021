51460356b6832b4aa224e3291d8b42fd492314b0
Cesar Garcia <d31tcjg@gmail.com>
2021-05-09 19:20:03 -0500

    Rebuild App after Crash

---
+++ src/components/EmptyHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen space-x-4 text-6xl text-black-300 font-extralight"
+  >
+    <div class="flex items-center justify-center space-x-4 animate-bounce">
+      <mdi:projector-screen-outline />
+      <h1 class="">Hello There</h1>
+      <noto:film-projector />
+    </div>
+  </div>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="bg-coolGray-500">
+    <div class="container flex items-center justify-between mx-auto">
+      <div class="flex items-center space-x-4">
+        <noto:film-projector class="w-16 h-16" />
+        <p
+          class="text-4xl tracking-wider text-transparent bg-gradient-to-r from-blue-400 to-black font-extralight bg-clip-text"
+        >
+          Movies<span class="font-bold">DB</span>
+        </p>
+      </div>
+      <div>
+        <div v-if="isAuthenticated" class="flex items-center space-x-4">
+          <p class="text-coolGray-200">Welcome {{ user?.displayName }}</p>
+          <button
+            @click="out"
+            class="px-8 py-2 font-semibold bg-red-600 rounded-full focus:ring-red-900 focus:ring-4 focus:outline-none text-coolGray-100 hover:bg-red-800"
+          >
+            Logout
+          </button>
+        </div>
+        <div v-else>
+          <button
+            @click="signIn"
+            class="px-8 py-2 font-semibold bg-green-600 rounded-full focus:ring-green-900 focus:ring-4 focus:outline-none text-coolGray-100 hover:bg-green-800"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { authentication } from "~/helpers/useFirebase";
+import { movies, page } from "~/helpers/useMovies";
+
+const { signIn, signOut, isAuthenticated, user } = authentication();
+
+const out = () => {
+  signOut();
+  page.value = 1;
+  movies.value = [];
+};
+</script>
